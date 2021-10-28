25637de5aa56444a29b413f52f65f2f04c2ec7bd
SeongMin Kim <57958360+SeongMin-K@users.noreply.github.com>
2021-10-28 13:14:43 +0900

    HighScoreBoard Answer

---
+++ HighScoreBoard.swift
@@ -0,0 +1,27 @@
+func newScoreBoard() -> [String: Int] {
+    return [:]
+}
+
+func addPlayer(_ scores: inout [String: Int], _ name: String, _ score: Int = 0) {
+    scores[name] = score
+}
+
+func removePlayer(_ scores: inout [String: Int], _ name: String) {
+    scores.removeValue(forKey: name)
+}
+
+func resetScore(_ scores: inout [String: Int], _ name: String) {
+    scores[name] = 0
+}
+
+func updateScore(_ scores: inout [String: Int], _ name: String, _ delta: Int) {
+    scores.updateValue(delta, forKey: name)
+}
+
+func orderByPlayers(_ scores: [String: Int]) -> [(String, Int)] {
+    scores.sorted { $0.0 < $1.0 }
+}
+
+func orderByScores(_ scores: [String: Int]) -> [(String, Int)] {
+    scores.sorted { $0.1 > $1.1 }
+}
